1cb7ceeed877718fcc0d49516ec32c510555488b
Adrian Rojo <rojoadrian16@gmail.com>
2022-11-24 11:05:06 -0600

    connect api to gameID

--- src/components/pages/GamePage.js
+++ src/components/pages/GamePage.js
@@ -1,44 +1,51 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import {useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
+
 
 import axios from 'axios'
 
 function GamePage(props){
     // Cheapshark Game ID 
-    const [gameId, setGameId] = useState('') 
-    const [idResponse,setIdResponse] = useState('')
-
+    const [idResponse,setIdResponse] = useState([])
+    const [price, setPrice] = useState("")
 
     const {id} = useParams()
     const game = props.apiResponse[id]
 
-    console.log(game)
-    if(!game){
-        return(
-            <h1>Oops! something went wrong</h1>
-        )
-    }
+    // console.log(game)
 useEffect(() =>{
     const getId = async() => {
         try{
-            const url = `https://www.cheapshark.com/api/1.0/games?id=${gameId}`
+            const url = `https://www.cheapshark.com/api/1.0/games?id=${game.gameID}`
             const response = await axios.get(url)
-            console.log(response.data)
-            setIdResponse([...idResponse, ...response.data])
+            console.log('response ',response.data)
+            setIdResponse([response.data])
+            // console.log(idResponse)
+            setPrice (response.data.cheapestPriceEver.price)
+            console.log('price', price)
         }catch(err){
             console.warn(err)
         }
     }
     getId()
+    // console.log('id ',idResponse)
+    // console.log('price-2', price)
 }, [])
+
+    if(!game){
+        return(
+            <h1>Oops! something went wrong</h1>
+        )
+    }
     return(
         <div>
             <h1>Game Page</h1>
             <h2>{game.external}</h2>
             <img src={game.thumb}/>
-            <p>{game.cheapest}</p>
+            <p>{price} </p>
             
+            <a href='' ></a>
         </div>
     )
 }
