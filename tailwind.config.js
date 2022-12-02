0f53d386b5df43ac4f1a238dac601f5167e806cf
Adrian Rojo <rojoadrian16@gmail.com>
2022-12-01 20:16:58 -0600

    finished setting up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
-const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// const colors = require('tailwindcss/colors')
   
-  module.exports = {
-    // ...
-    theme: {
-      extend: {
-        colors: {
-          teal: colors.teal,
-          cyan: colors.cyan,
-        },
-      },
-    },
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
+//   module.exports = {
+//     // ...
+//     theme: {
+//       extend: {
+//         colors: {
+//           teal: colors.teal,
+//           cyan: colors.cyan,
+//         },
+//       },
+//     },
+//     plugins: [
+//       // ...
+//       require('@tailwindcss/forms'),
+//       require('@tailwindcss/aspect-ratio'),
+//     ],
+//   }
